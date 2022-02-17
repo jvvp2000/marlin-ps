fec7be30859e76e9d0c64546c1ac3c9e7e260881
Joao Pinto <comlasers@hotmail.com>
2022-02-17 02:54:45 -0300

    website in progress

---
+++ Etapa03/ANC-website/js/script.js
@@ -0,0 +1,28 @@
+// tns e uma ferramenta do tiny slider
+let slider = tns({
+    container: ".slide_all",
+    "slideBy": "1",
+    "speed": 500,
+    "nav": false,
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    responsive: {
+        1600: {
+            items: 4,
+            gutter: 20
+        },
+        1024: {
+            items: 3,
+            gutter: 20
+        },
+        768: {
+            items: 2,
+            gutter: 20
+        },
+        424: {
+            items: 1,
+            gutter: 20
+        }
+    }
+})
